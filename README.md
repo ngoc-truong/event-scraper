7e5c0ca4b40817a1b932cc24a08b43e0d1137413
Ngoc Truong <ngoc.truong@posteo.de>
2022-06-24 22:48:50 +0200

    Click load more button endlessly

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Event Scraper
 
-In this project I built an event scraper which will scrape events from Facebook groups.
+In this project I built an event scraper which will scrape events from my Facebook groups.
 
 ## Problem Statement
 
 Since I am a passionate Lindy Hop dancer I want to be up to date on current events in my city. However, since there are several groups, it is impossible to keep track on where and when events happen.
 
-This bot scrapes all events from all subscribed groups.
+This bot scrapes all events from all of my personal subscribed groups.
--- index.js
+++ index.js
@@ -6,22 +6,56 @@ const cookieButton = '[data-cookiebanner="accept_only_essential_button"]';
 const emailInput = '[name="email"]';
 const passwordInput = '[name="pass"]';
 const loginButton = '[name="login"]';
+const showMoreButton = '[aria-label="Mehr anzeigen"]';
+
+// Groups
+const groupLink = "https://www.facebook.com/groups/296668743081/events";
 
 // Puppeteer
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+  const browser = await puppeteer.launch({ headless: false });
 
   const page = await browser.newPage();
+
+  // Login
   await page.goto("https://www.facebook.com", { waitUntil: "networkidle2" });
   await page.waitForSelector(cookieButton);
   await page.click(cookieButton);
   await page.waitForSelector(emailInput);
   await page.waitForSelector(passwordInput);
   await page.waitForSelector(loginButton);
-  await page.type(emailInput, process.env.EMAIL);
-  await page.type(passwordInput, process.env.PASS);
+  await page.type(emailInput, process.env.EMAIL, { delay: 120 });
+  await page.type(passwordInput, process.env.PASS, { delay: 120 });
   await page.click(loginButton);
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(2000);
 
+  // Click load more button as long as possible
+  await page.goto(groupLink, { waitUntil: "networkidle2" });
+
+  // let loadMoreVisible = await isButtonVisible(page, showMoreButton);
+
+  for (let i = 0; i < 5; i++) {
+    await page.waitForSelector(showMoreButton);
+    await page.click(showMoreButton).catch(() => {});
+    await console.log(`Das ist das ${i}. Mal jetze.`);
+  }
+
+  // while (loadMoreVisible) {
+  //   await page.click(showMoreButton).catch(() => {});
+  //   loadMoreVisible = await isButtonVisible(page, showMoreButton);
+  // }
+
+  await page.waitForTimeout(5000);
   await browser.close();
 })();
+
+// Helper functions
+const isButtonVisible = async (page, cssSelector) => {
+  let visible = true;
+  await page
+    .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
+    .catch(() => {
+      visible = false;
+    });
+  return visible;
+};
